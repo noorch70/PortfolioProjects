b9352977e41b95b8657925ebbb5fabc014522875
noorch70 <85807314+noorch70@users.noreply.github.com>
2021-09-28 10:16:51 -0400

    Update amazonwebscrapping.py

--- amazonwebscrapping.py
+++ amazonwebscrapping.py
@@ -1,3 +1,4 @@
+#Step1 : Importing libraries
 from bs4 import BeautifulSoup
 import requests
 import datetime
@@ -5,7 +6,7 @@ import time
 import smtplib
 import csv
 from decimal import Decimal
-# #connect to website
+ #Step2 : Connecting to website and pulling out title and price of item (since the project is automated, the csv file for price tracking is created the first time and then appended through the function)
 # url='https://www.amazon.com/Conair-Double-Ceramic-Curling-1-inch/dp/B07CKKGR83/ref=sr_1_5?dchild=1&keywords=curling+iron&qid=1632600137&sr=8-5'
 # headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.54 Safari/537.36","Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
 # page=requests.get(url,headers=headers)
@@ -21,22 +22,24 @@ from decimal import Decimal
 
 
 # print(price)
-
+#generating date log
 # today=datetime.date.today()
 # header=['Title','Price','Date']
 # data=[title,price,today]
+#Step3: Creating a csv file
 # with open('amazondata1.csv','w+',newline='',encoding='UTF8') as f:
 #  writer=csv.writer(f)
 #  writer.writerow(header)
 #  writer.writerow(data)
 
  
-#  #now appending the data to csv
+#  #Step4: now appending the data to csv
 
 # with open('amazondata.csv','a+',newline='',encoding='UTF8') as f:
 #  writer=csv.writer(f)
 #  writer.writerow(data)
 #  #now putting this together in a function
+#the above code is now just put in function 'check_price'
 def check_price():
  #connect to website
  url='https://www.amazon.com/Conair-Double-Ceramic-Curling-1-inch/dp/B07CKKGR83/ref=sr_1_5?dchild=1&keywords=curling+iron&qid=1632600137&sr=8-5'
@@ -50,9 +53,11 @@ def check_price():
 #pulling price
  price=soup2.find(id='price_inside_buybox').get_text()
  price=price.strip()
+ #generating datelog
  today=datetime.date.today()
  header=['Title','Price','Date']
  data=[title,price,today]
+ #appending the data to csv file
  with open('amazondata.csv','a+',newline='',encoding='UTF8') as f:
   writer=csv.writer(f)
   writer.writerow(data)
@@ -60,7 +65,7 @@ def check_price():
  return price_int
 
 
-
+#Step5 : send_mail function will inform me via email if the price of the item goes below $15
 def send_mail():
     server = smtplib.SMTP_SSL('smtp.gmail.com',465)
     server.ehlo()
